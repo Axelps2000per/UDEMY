fc7b72b7304248d5b161c14f746d8ce3aaf7b0f6
M Axel PS <118088449+Axelps2000per@users.noreply.github.com>
2024-05-08 00:41:22 +0300

    Add files via upload

---
+++ Tema 14/07-FT.js
@@ -0,0 +1,34 @@
+function names(){
+     
+    Valor = document.getElementById("names");
+}
+    
+    
+    
+    function lastNames(){
+    
+}
+
+function phoneNumber(){
+    
+}
+
+function mail(){
+    
+}
+
+function submit(){
+    
+    while (Valor.length < 4){
+        prompt("Por favor escriba un nombre válido");
+    }
+ if(Valor.length = 0) 
+    {
+        document.getElementById("parr").style.backgroundColor="#C73659";
+    }   else if (Valor > 3) 
+        {
+            document.getElementById("parr").style.backgroundColor="#24da1e";   
+        }    
+}
+
+
